50177d82ccfe60d88e4252b5f25ed39d1ca48b26
phillipmhart <phillip.hart@gmail.com>
2026-08-25 11:05:37 -0400

    Publish offline mobile game

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = "staff-of-ministry-v1";
+const FILES = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
+self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+  if (event.request.method === "GET" && new URL(event.request.url).origin === location.origin) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+  return response;
+}).catch(() => caches.match("./index.html")))));
