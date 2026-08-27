87ea7c8bd12da74e3440f141652c28d4685e712d
phillipmhart <phillip.hart@gmail.com>
2026-08-26 21:29:51 -0400

    Fix offline cache updates for mobile players

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
-const CACHE = "staff-of-ministry-v2";
+const CACHE = "staff-of-ministry-v8";
 const FILES = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
-self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
-  if (event.request.method === "GET" && new URL(event.request.url).origin === location.origin) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
-  return response;
-}).catch(() => caches.match("./index.html")))));
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).then(response => {
+    if (new URL(event.request.url).origin === location.origin) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+    return response;
+  }).catch(() => caches.match(event.request).then(hit => hit || caches.match("./index.html"))));
+});
